0424515334557b0debb9a4d0c43c9cb0b13c569e
iTeckno <86756836+iTeckno@users.noreply.github.com>
2021-10-20 01:29:47 -0400

    Add files via upload

---
+++ hypotenuse/index.php
@@ -0,0 +1,45 @@
+<?php
+
+$calculationWasRequested = $_GET['calculateRequest'];
+
+$firstSide = 3;
+$secondSide = 4;
+
+if (isset($calculationWasRequested)) {
+  $result = sqrt(pow($firstSide, 2) + pow($secondSide, 2));
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+  <meta charset='UTF-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+  <title>Hipotenusa</title>
+</head>
+
+<body>
+  <main>
+    <div>
+      <h2>
+        This webpage calculates the hypotenuse of <strong><?php echo "$firstSide cm and $secondSide cm"; ?></strong>.
+      </h2>
+      <?php
+      if (!isset($result)) {
+        echo '<p>Click <em>"calculate"</em> to see the result!</p>';
+      } else {
+        echo "<p>Resultado: <strong>$result</strong></p>";
+      }
+      ?>
+      <form method='GET' action=<?php echo $_SERVER['PHP_SELF']; ?>>
+        <button type='submit' name='calculateRequest'>Calculate</button>
+      </form>
+      <p>Nombre - Cedula - N1013</p>
+    </div>
+  </main>
+</body>
+
+</html>
